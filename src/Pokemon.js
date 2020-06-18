601a319a8cfe507f41b34da28dc765ed56c995c9
Erik <elj830@gmail.com>
2020-06-17 19:53:45 -0500

    Added All vs Individual

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -9,30 +9,29 @@ function Pokemon(props)
         fetch(`https://pokeapi.co/api/v2/pokemon/${props.data}`)
     .then(result => result.json())
     .then(setPokemon);
-    }
-    
-    )
+    })
     
     let imgSrc = "https://pokeres.bastionbot.org/images/pokemon/" + pokemon['id'] + '.png'
     if (!pokemon['abilities']){
             return null
         }
 
+    let types = pokemon['types'].map(types => <li>{types['type']['name'][0].toUpperCase() + types['type']['name'].slice(1)}</li>)  
+    let abilities = pokemon['abilities'].map(ability => <li>{ability['ability']['name'][0].toUpperCase() + ability['ability']['name'].slice(1)}</li>)
+
+
     return(
         <div className="info">
             {<img src={imgSrc} id='pokemon-img'></img>}
-            
-            
-            <ul>
-                Abilities 
-                <li>{pokemon['abilities'].map(ability => ability['ability']['name'][0].toUpperCase() + ability['ability']['name'].slice(1) + ' ')}</li>
+            <ul>Abilities: 
+                {/* Abilities 
+                <li>{pokemon['abilities'].map(ability => ability['ability']['name'][0].toUpperCase() + ability['ability']['name'].slice(1) + ' ')}</li> */}
+                {abilities}
             </ul>
             <ul>
                 Type(s)
-                <li>{pokemon['types'].map(types => types['type']['name'][0].toUpperCase() + types['type']['name'].slice(1) + ' ')}</li>
-                
-                
-
+                {/* <li>{pokemon['types'].map(types => types['type']['name'][0].toUpperCase() + types['type']['name'].slice(1) + ' ')}</li> */}
+                {types}
             </ul>
 
         </div>
--- src/SearchPokemon.js
+++ src/SearchPokemon.js
@@ -8,7 +8,9 @@ class SearchPokemon extends React.Component
         super(props)
         this.state = { value: "",
                         newValue: "",
-                        newData: {}
+                        newData: {},
+                        imgArr: [],
+                        checkVal: 0,
                     };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -27,13 +29,31 @@ class SearchPokemon extends React.Component
     {
         event.preventDefault();
         this.setState({value: this.state.newValue});
+        this.setState({checkVal: 0})
+        
     }
-    
+    handleAll = (event) => {
+        event.preventDefault();
+        let imgArray = []
+        for (let i = 1; i < 151; i++){
+           imgArray.push(<img src={`https://pokeres.bastionbot.org/images/pokemon/${i}.png`} id='random-poke'></img>)}
+           this.setState({imgArr: imgArray})
+        this.setState({checkVal: 1})   
+        
+        
+
+    }
     
 
     render() {
-
-
+        let returnData
+        if (this.state.checkVal === 1){
+            returnData = this.state.imgArr
+        }
+        if (this.state.checkVal === 0){
+            returnData = <Pokemon data={this.state.value.toLowerCase()} />
+        }
+        
         return (
           <div>
             <form onSubmit={this.handleSubmit}>
@@ -44,10 +64,9 @@ class SearchPokemon extends React.Component
                 </label>
                 <input type="submit" value="Submit"  />
             </form>
-            
+            <button onClick={this.handleAll}>Show All</button>
           <div className='container'>
-                <h2>{this.props.name}</h2>
-                <Pokemon data={this.state.value.toLowerCase()} />
+                {returnData}
             </div>
           </div>
         );
